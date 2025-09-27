0e7f8ac7a134f8398558d82b573104494844b89a
Cristian <criscamcr7@gmail.com>
2025-09-27 15:27:50 -0500

    Metatags

--- alitasLocas/src/modules/landing/utils.js
+++ alitasLocas/src/modules/landing/utils.js
@@ -1,10 +1,10 @@
 // Imports de imágenes para que Vite las procese correctamente
-import soloParaTiImg from '../../../assets/Solo para ti.webp';
-import duoDeliveryImg from '../../../assets/Duo delivery.webp';
-import familiarImg from '../../../assets/Familiar en casa.webp';
-import oficinaImg from '../../../assets/Oficina - Amigos.webp';
-import parrandaImg from '../../../assets/Parranda en casa.webp';
-import papasImg from '../../../assets/Papas.webp';
+import soloParaTiImg from '../../assets/images/Solo para ti.webp';
+import duoDeliveryImg from '../../assets/images/Duo delivery.webp';
+import familiarImg from '../../assets/images/Familiar en casa.webp';
+import oficinaImg from '../../assets/images/Oficina - Amigos.webp';
+import parrandaImg from '../../assets/images/Parranda en casa.webp';
+import papasImg from '../../assets/images/Papas.webp';
 
 export const products = [
   {
