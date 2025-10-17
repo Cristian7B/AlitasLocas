01e37046b472d2f529de18d354fd8cd3912abbac
Cristian <criscamcr7@gmail.com>
2025-10-17 15:41:23 -0500

    Add: Down arrow icon

--- alitasLocas/src/modules/landing/components/header/Header.jsx
+++ alitasLocas/src/modules/landing/components/header/Header.jsx
@@ -2,29 +2,33 @@ import { Buttons } from "./Buttons";
 import { Nav } from "./Nav";
 import showIcon from "../../../../assets/images/ShowIcon.svg"
 import landingImage from "../../../../assets/images/LandingImg.webp"
+import downArrow from "../../assets/chevron-down-outline.svg"
     
 export function Header() {
     return (
         <>
             <header>
-                <div className="headerText">
-                <Nav/>
-                    <div className="importantText">
-                        <img src={showIcon} alt="Important icon" />
-                        <p>Descubre una nueva adicción</p>
+                <section className="containerHeader">
+                    <div className="headerText">
+                        <Nav/>
+                        <div className="importantText">
+                            <img src={showIcon} alt="Important icon" />
+                            <p>Descubre una nueva adicción</p>
+                        </div>
+                        <h1 className="titleText">
+                            ¡Las mejores alitas de Bogotá!
+                        </h1>
+                        <p className="descriptionText">
+                        Alitas llenas de sabor, preparadas al momento, listas para volar directo a tu paladar y el toque perfecto para compartir con quien quieras.
+                        </p>
+                        <Buttons/>
+                        <p className="locationText">Por ahora en: Bogotá</p>
                     </div>
-                    <h1 className="titleText">
-                        ¡Las mejores alitas de Bogotá!
-                    </h1>
-                    <p className="descriptionText">
-                       Alitas llenas de sabor, preparadas al momento, listas para volar directo a tu paladar y el toque perfecto para compartir con quien quieras.
-                    </p>
-                    <Buttons/>
-                    <p className="locationText">Por ahora en: Bogotá</p>
-                </div>
-                <div className="headerImage">
-                    <img src={landingImage} alt="Header image" />
-                </div>
+                    <div className="headerImage">
+                        <img src={landingImage} alt="Header image" />
+                    </div>
+                </section>
+                <img className="downArrow" src={downArrow} alt="" />
             </header>
         </>
     )
--- alitasLocas/src/modules/landing/components/header/Nav.jsx
+++ alitasLocas/src/modules/landing/components/header/Nav.jsx
@@ -9,3 +9,4 @@ export function Nav() {
         </nav>
     )
 }
+
